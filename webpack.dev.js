d3ca2c0d14af861c21aac473e021e76a3097cb04
pacleonardo97 <pacleonardo97@gmail.com>
2020-09-24 23:15:47 -0300

    adjust

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const path = require('path');
 const dotenv = require('dotenv-webpack');
+const storeName = 'telhanorte';
 
 module.exports = merge(common, {
   mode: 'development',
@@ -13,6 +14,24 @@ module.exports = merge(common, {
     port: 3333,
     historyApiFallback: true,
     writeToDisk: true,
+    open: {
+      app: 'chrome'
+    },
+    proxy: { 
+      // '/app': {
+      //   target: 'telhanorte.vtexcommercestable.com.br',
+      //   secure: false,
+      //   pathRewrite: { '^/app': '' }
+      // }
+      '/app/' : 'http://localhost:3000'
+    },
+    before: (app, server, compiler) => {
+      app.get('/app/', (req, res) => {
+        res.redirect('https://telhanorte.vtexcommercestable.com.br');
+      });
+    },
   },
   plugins: [new dotenv()],
 });
+
+// 'telhanorte.vtexcommercestable.com.br'
